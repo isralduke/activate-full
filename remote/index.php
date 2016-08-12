71c73b5db04009a30852babf7a9819da8839c320
isral Duke <isralduke@gmail.com>
2016-08-11 19:56:12 -0500

    added some stuff
    changed menu to be fixed at bottom for mobile sizes, added headshot,
    some socme links

--- remote/index.php
+++ remote/index.php
@@ -55,7 +55,7 @@
 						<img class="nav-img" src="assets/img/rocket.svg" alt=""></a>
 				</li>
 				<li><a data-scroll href="#about-activate">about</a></li>
-				<li><a data-scroll href="#speakers">speakers <br class="show-mobile" />&amp; panels</a></li>
+				<li><a data-scroll href="#speakers">speakers &amp; panels</a></li>
 				<li><a data-scroll href="#venue">venue</a></li>
 				<li><a data-scroll href="#sponsors">sponsors</a></li>
 				<li><a data-scroll href="#organizers">organizers</a></li>
@@ -333,44 +333,47 @@
 				<section class="section">
 					<h3>activating your reach</h3>
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris turpis magna, fringilla eget congue a, vulputate vel felis. Fusce hendrerit urna in lectus tristique nec commodo est luctus. Nullam malesuada lobortis diam ut placerat. Vestibulum cursus ultrices eros sed euismod. Suspendisse potenti. Donec vehicula venenatis rhoncus. Etiam pretium pretium aliquet. Aliquam erat volutpat.</p>
+					<!-- lynsey -->
 					<div class="speaker-panel">
 						<h4>lynsey jordan gwin</h4>
 						<p>envoc</p>
 						<div class="speaker-socials">
-							<a href="fb">
+							<a href="//www.facebook.com/lynseydesign" target="_blank" title="See Lynsey Jordan Gwin on Facebook">
 								<?php include('partials/fb.html'); ?>
 							</a>
-							<a href="tw">
+							<a href="//twitter.com/lynseydesign" target="_blank" title="See Lynsey Jordan Gwin on Twitter">
 								<?php include('partials/tw.html'); ?>
 							</a>
-							<a href="ig">
+							<a href="//www.instagram.com/lynseydesign/" target="_blank" title="See Lynsey Jordan Gwin on Instagram">
 								<?php include('partials/ig.html'); ?>
 							</a>
 						</div>
 					</div>
+					<!-- quinton -->
 					<div class="speaker-panel">
 						<h4>quinton jason jr</h4>
 						<p>x design</p>
 						<div class="speaker-socials">
-							<a href="fb">
+							<a href="//www.facebook.com/quinton.jason" target="_blank" title="See Quinton Jason Jr on Facebook">
 								<?php include('partials/fb.html'); ?>
 							</a>
-							<a href="tw">
+							<a href="//twitter.com/quintonjasonjr" target="_blank" title="See Quinton Jason Jr on Twitter">
 								<?php include('partials/tw.html'); ?>
 							</a>
-							<a href="ig">
+							<a href="//www.instagram.com/quintonjasonjr/" target="_blank" title="See Quinton Jason Jr on Instagram">
 								<?php include('partials/ig.html'); ?>
 							</a>
 						</div>
 					</div>
+					<!-- isral -->
 					<div class="speaker-panel">
 						<h4>isral duke</h4>
 						<p>stun design</p>
 						<div class="speaker-socials">
-							<a href="//www.twitter.com/isralduke" target="_blank">
+							<a href="//www.twitter.com/isralduke" target="_blank" title="See isral Duke on Twitter">
 								<?php include('partials/tw.html'); ?>
 							</a>
-							<a href="//www.instagram.com/isralduke" target="_blank">
+							<a href="//www.instagram.com/isralduke" target="_blank" title="See isral Duke on Instagram">
 								<?php include('partials/ig.html'); ?>
 							</a>
 						</div>
@@ -379,9 +382,9 @@
 			</div>
 			<div class="col col-50">
 				<section class="speaker-panel-photos">
-					<img class="panel-photo" src="assets/img/headshots/isral-c-duke.jpg" alt="">
-					<img class="panel-photo" src="assets/img/headshots/isral-c-duke.jpg" alt="">
-					<img class="panel-photo" src="assets/img/headshots/isral-c-duke.jpg" alt="">
+					<img class="panel-photo" src="assets/img/headshots/isral-c-duke.jpg" alt="Lynsey Jordan Gwin, Designer, Front End Developer, Activate Conference Organizer">
+					<img class="panel-photo" src="assets/img/headshots/quinton-jason-jr-activate-conference-organizer.jpg" alt="Quinton Jason, Jr., Front End Developer, SVG Animator, Activate Conference Organizer">
+					<img class="panel-photo" src="assets/img/headshots/isral-duke-activate-conference-organizer.jpg" alt="Isral Duke, Designer, Front End Developer, Activate Conference Organizer">
 				</section>
 			</div>
 		</article>
